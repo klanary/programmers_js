22a1c831577915bb81374824a9f1bfc6e790f1cb
klanary <134908413+klanary@users.noreply.github.com>
2025-08-26 17:34:34 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,10 @@
+
+def solution(word):
+    answer = 0
+    weight=[781,156,31,6,1]
+    alpha=['A','E','I','O','U']
+    for i in range(len(word)):
+        num=alpha.index(word[i])
+        answer+=weight[i]*num
+    answer+=len(word)
+    return answer
